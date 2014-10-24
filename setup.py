8e626bbe1078e5e2b9b148d7bce5f0417340d039
Colin Alston <colin.alston@gmail.com>
2014-10-24 10:24:20 +0200

    Start the next release version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="tensor",
-    version='0.0.7',
+    version='0.1.0',
     url='http://github.com/calston/tensor',
     license='MIT',
     description="A Twisted based monitoring agent for Riemann",
