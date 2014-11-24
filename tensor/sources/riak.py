64a6c334076f62fbc9ddb422c822968b4f8e6a1a
Jeremy Thurgood <firxen@gmail.com>
2014-11-24 18:12:20 +0200

    Add basic Riak stats source.

---
+++ tensor/sources/riak.py
@@ -0,0 +1,73 @@
+"""
+.. module:: riak
+   :platform: Any
+   :synopsis: A source module for Riak metrics
+
+.. moduleauthor:: Jeremy Thurgood <firxen@gmail.com>
+"""
+
+import json
+
+from twisted.internet import defer, reactor
+from twisted.web.client import Agent
+from twisted.web.http_headers import Headers
+
+from zope.interface import implements
+
+from tensor.interfaces import ITensorSource
+from tensor.objects import Source
+
+from tensor.utils import BodyReceiver
+
+
+class RiakStats(Source):
+    """Returns GET/PUT rates for a Riak node
+
+    **Configuration arguments:**
+
+    :url: Riak stats URL
+    :type url: str.
+    :useragent: User-Agent header to use
+    :type useragent: str.
+
+    **Metrics:**
+
+    :(service name).latency: Time to complete request
+    """
+
+    implements(ITensorSource)
+
+    @defer.inlineCallbacks
+    def _get_stats_from_node(self):
+        agent = Agent(reactor)
+
+        url = self.config.get('url', 'http://%s:8098/stats' % self.hostname)
+        ua = self.config.get('useragent', 'Tensor Riak stats checker')
+
+        headers = Headers({'User-Agent': [ua]})
+        request = yield agent.request('GET', url, headers)
+
+        if request.length:
+            d = defer.Deferred()
+            request.deliverBody(BodyReceiver(d))
+            b = yield d
+            body = b.read()
+        else:
+            body = ""
+
+        defer.returnValue(json.loads(body))
+
+    @defer.inlineCallbacks
+    def get(self):
+        stats = yield self._get_stats_from_node()
+        get_rate = stats['node_gets'] / 60.0
+        put_rate = stats['node_puts'] / 60.0
+
+        defer.returnValue([
+            self.createEvent(
+                'ok', 'GETs per second for past minute', get_rate,
+                prefix="gets_per_second"),
+            self.createEvent(
+                'ok', 'PUTs per second for past minute', put_rate,
+                prefix="puts_per_second"),
+        ])
--- tensor/tests/test_sources.py
+++ tensor/tests/test_sources.py
@@ -1,9 +1,12 @@
+import json
 import socket
 
 from twisted.trial import unittest
-from twisted.internet import defer
+from twisted.internet import defer, endpoints, reactor
+from twisted.web import server, static
 
 from tensor.sources.linux import basic, process
+from tensor.sources import riak
 
 
 class TestLinuxSources(unittest.TestCase):
@@ -58,3 +61,61 @@ class TestLinuxSources(unittest.TestCase):
             {'interval': 1.0, 'service': 'ps', 'ttl': 60}, self._qb, None)
 
         yield s.get()
+
+
+class TestRiakSources(unittest.TestCase):
+    def _qb(self, result):
+        pass
+
+    def start_fake_riak_server(self, stats):
+        def cb(listener):
+            self.addCleanup(listener.stopListening)
+            return listener
+
+        data = static.Data(json.dumps(stats), 'application/json')
+        data.isLeaf = True
+        site = server.Site(data)
+        endpoint = endpoints.TCP4ServerEndpoint(reactor, 0)
+        return endpoint.listen(site).addCallback(cb)
+
+    def make_riak_stats_source(self, config_overrides={}):
+        config = {
+            'interval': 1.0,
+            'service': 'riak',
+            'ttl': 60,
+            'hostname': 'localhost',
+        }
+        config.update(config_overrides)
+        return riak.RiakStats(config, self._qb, None)
+
+    @defer.inlineCallbacks
+    def test_riak_stats_zeros(self):
+        listener = yield self.start_fake_riak_server({
+            'node_gets': 0,
+            'node_puts': 0,
+        })
+        addr = listener.getHost()
+        url = 'http://localhost:%s/' % (addr.port,)
+        s = self.make_riak_stats_source({'url': url})
+
+        [gets, puts] = yield s.get()
+        self.assertEqual(gets.service, "riak.gets_per_second")
+        self.assertEqual(gets.metric, 0.0)
+        self.assertEqual(puts.service, "riak.puts_per_second")
+        self.assertEqual(puts.metric, 0.0)
+
+    @defer.inlineCallbacks
+    def test_riak_stats(self):
+        listener = yield self.start_fake_riak_server({
+            'node_gets': 150,
+            'node_puts': 45,
+        })
+        addr = listener.getHost()
+        url = 'http://localhost:%s/' % (addr.port,)
+        s = self.make_riak_stats_source({'url': url})
+
+        [gets, puts] = yield s.get()
+        self.assertEqual(gets.service, "riak.gets_per_second")
+        self.assertEqual(gets.metric, 2.5)
+        self.assertEqual(puts.service, "riak.puts_per_second")
+        self.assertEqual(puts.metric, 0.75)
