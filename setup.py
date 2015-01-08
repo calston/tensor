cd55003a2ffcd8656879141a7cdfcaab365470f1
Colin Alston <colin.alston@gmail.com>
2015-01-08 21:31:38 +0200

    Increment build version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="tensor",
-    version='0.1.16',
+    version='0.1.17',
     url='http://github.com/calston/tensor',
     license='MIT',
     description="A Twisted based monitoring agent for Riemann",
