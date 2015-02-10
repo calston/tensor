649fa0d8ac8f609d6dffd5c6939c76cae3720db4
Colin Alston <colin.alston@gmail.com>
2015-02-10 21:51:35 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="tensor",
-    version='0.2.6',
+    version='0.2.7',
     url='http://github.com/calston/tensor',
     license='MIT',
     description="A Twisted based monitoring agent for Riemann",
