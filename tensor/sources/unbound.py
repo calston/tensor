7cf3138ab205ad8181bfde94119314cf2c50d6f9
Colin Alston <colin.alston@gmail.com>
2016-03-11 10:34:55 +0200

    Add unbound DNS stats source

---
+++ tensor/sources/unbound.py
@@ -0,0 +1,55 @@
+from zope.interface import implements
+
+from twisted.internet import defer
+from twisted.python import log
+
+from tensor.interfaces import ITensorSource
+from tensor.objects import Source
+from tensor.utils import fork
+
+
+class Stats(Source):
+    """Returns stats from unbound-control
+
+    **Configuration arguments:**
+
+    :param executable: Path to unbound-control executable 
+                       (default: /usr/sbin/unbound-control)
+    :type executable: str.
+
+    """
+    implements(ITensorSource)
+
+    def __init__(self, *a, **kw):
+        Source.__init__(self, *a, **kw)
+
+        self.uc = self.config.get('executable', '/usr/sbin/unbound-control')
+
+    @defer.inlineCallbacks
+    def _get_uc_stats(self):
+        out, err, code = yield fork(self.uc, args=('stats', ))
+        
+        if code == 0:
+            defer.returnValue(out.strip('\n').split('\n'))
+        else:
+            log.msg('Error running unbound-control: ' + repr(err))
+            defer.returnValue([])
+
+    @defer.inlineCallbacks
+    def get(self):
+        events = []
+        
+        stats = yield self._get_uc_stats()
+
+        for row in stats:
+            key, val = row.split('=')
+            
+            try:
+                val = float(val)
+            except:
+                # Not a number
+                continue 
+
+            events.append(self.createEvent('ok', key, val, prefix=key))
+
+        defer.returnValue(events)
